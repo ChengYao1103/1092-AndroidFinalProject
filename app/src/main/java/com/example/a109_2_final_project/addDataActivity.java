8333c57d23397730127ffe1e97bb58a9f8089ed5
108590027 <t108590027@ntut.org.tw>
2021-06-12 15:30:06 +0800

    add on account page

---
+++ app/src/main/java/com/example/a109_2_final_project/addDataActivity.java
@@ -0,0 +1,56 @@
+package com.example.a109_2_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class addDataActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner mSpin;
+    private int mType;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_data);
+
+        //參考04.2內容
+        mSpin =findViewById(R.id.spinner_type);
+        if (mSpin != null) {
+            mSpin.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.labels_array,
+                android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        if (mSpin != null) {
+            mSpin.setAdapter(adapter);
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String spinnerLabel = parent.getItemAtPosition(position).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
